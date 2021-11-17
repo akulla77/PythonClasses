a40e62bfa014f774fc6e09a86ffae86aa6e9f3c9
Konstantin Dobrychev <dobrychev.konstantin@gmail.com>
2021-11-17 19:50:33 +0700

    notes list replaced to dict

--- 04/main.py
+++ 04/main.py
@@ -1,27 +1,34 @@
 from dataclasses import dataclass
-from typing import List
+from typing import Dict, Iterable
 
 
 @dataclass
 class Note:
+    id: int
     author: str
     message: str
 
 
-GLOBAL_NOTES = []
+GLOBAL_NOTES: Dict[int, Note] = {}
 
 
-def get_all_notes() -> List[Note]:
-    return GLOBAL_NOTES
+def get_all_notes() -> Iterable[Note]:
+    return GLOBAL_NOTES.values()
 
 
 def add_note(note: Note):
-    GLOBAL_NOTES.append(note)
+    GLOBAL_NOTES[note.id] = note
 
 
-def remove_note(index: int):
-    GLOBAL_NOTES.pop(index)
+def remove_note(note_id: int):
+    del GLOBAL_NOTES[note_id]
 
 
 if __name__ == '__main__':
-    pass
+    add_note(Note(0, 'user_1', 'note_1'))
+    add_note(Note(1, 'user_1', 'note_2'))
+
+    print(get_all_notes())
+
+    remove_note(0)
+    print(get_all_notes())
