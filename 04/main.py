16c66e6c56cedafc4d776b734d43592ded423949
Konstantin Dobrychev <dobrychev.konstantin@gmail.com>
2021-11-17 19:43:36 +0700

    added actions

--- 04/main.py
+++ 04/main.py
@@ -1,10 +1,26 @@
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass
 class Note:
     author: str
     message: str
+
+
+GLOBAL_NOTES = []
+
+
+def get_all_notes() -> List[Note]:
+    return GLOBAL_NOTES
+
+
+def add_note(note: Note):
+    GLOBAL_NOTES.append(note)
+
+
+def remove_note(index: int):
+    GLOBAL_NOTES.pop(index)
 
 
 if __name__ == '__main__':
